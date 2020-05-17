d20c10a90cbdaf0c6f32a9081bee07768c629fd0
Gabriela Sanchez <gaby.peta@gmail.com>
2020-05-17 05:46:24 +0000

    Change details

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const chalk = require("chalk");
-
-
 const randomColor = require("randomcolor");
 const color = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
 });
-
-
 console.log(chalk.hex(color)('##############################\n##############################\n##############################\n##########  ' + `${color}` + '  #########\n##############################\n##############################\n##############################\n'));
 
 
